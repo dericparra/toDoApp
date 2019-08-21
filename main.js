476fd534ccfc22be252dc1f6fff5f31da81dcfe2
DericParra <parradeko@gmail.com>
2019-08-21 13:12:00 -0300

    terminei

---
+++ main.js
@@ -0,0 +1,27 @@
+var getInput = document.querySelector('[data-input]')
+var button = document.querySelector('[data-button]')
+var result = document.querySelector('[data-res]')
+var list = document.querySelector('[data-lis]')
+var toDo = [];
+
+function createList(){
+    let itemInput = getInput.value
+    let element = document.createElement('li')
+    let text = document.createTextNode(itemInput)
+    let elementButton = document.createElement('button')
+    elementButton.setAttribute('data-del', '')
+    let textButton = document.createTextNode('X')
+    element.append(text)
+    list.append(element)
+    elementButton.append(textButton)
+    element.append(elementButton)
+    removeElement(elementButton)
+}
+
+button.addEventListener('click', createList)
+
+function removeElement(element){
+    element.addEventListener('click', function(){
+        this.parentElement.innerHTML = ''
+    })
+}
